424e3c12840f335c325c29c006689f963fcaa519
Nasuh Emre ATEŞSOY <emreatessoy@gmail.com>
2021-02-06 00:40:40 +0300

    Option query fixed

--- Server/graphql/resolvers/queries/OptionQuery.js
+++ Server/graphql/resolvers/queries/OptionQuery.js
@@ -1,6 +1,6 @@
 const OptionValue = {
   OptionValues: async (parent, args, { OptionValue }) => {
-    return await OptionValue.find({ OptionsId: parent.id });
+    return await OptionValue.find({ OptionsId: parent.id, Status: 1 });
   },
 };
 
