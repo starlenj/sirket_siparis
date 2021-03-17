ad23aeeab2aa3261a01ba5a4353ccfd9893bb01a
Nasuh Emre ATEŞSOY <emreatessoy@gmail.com>
2021-03-17 13:38:53 +0300

    bekleyen siparis sms bildirim
    bekleyen siparis sms bildirim

---
+++ Server/helpers/waitingOrders.js
@@ -0,0 +1,34 @@
+const model = require("../models/OrderHeader");
+const moment = require('moment');
+const axios = require('axios');
+module.exports = {
+    async main() {
+        setInterval(() => {
+            this.GetWaitingOrderList();
+        }, 30000)
+
+    },
+    async GetWaitingOrderList() {
+        try {
+            const today = moment().startOf('day')
+
+            let response = await model.find({
+                OrderStatus: 0, Date: {
+                    $gte: today.toDate(),
+                    $lte: moment(today).endOf('day').toDate()
+                }
+            });
+            if (response.length > 0) {
+                var Mesaj = ` ${response[0].SubeId} subesinde onay bekleyen bir adet siparis var . Musteri bilgisi ${response[0].CustomerName}`
+                var GSM = `5301700974,53233217,53994747`
+                var url = "https://api.netgsm.com.tr/sms/send/get/?usercode=" + process.env.SMS_USERNAME + "&password=" + process.env.SMS_PASSWORD + "&gsmno=" + GSM + "&message=" + Mesaj + "&msgheader=HMBRGR";
+                let SmsResponse = await axios.get(encodeURI(url));
+                console.log(`sms gitti`, SmsResponse);
+            }
+
+        } catch {
+            console.error("SMS ERROR")
+        }
+
+    }
+}
